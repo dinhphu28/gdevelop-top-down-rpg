cea379c536b124df8b0cd07298a4d59d571b6ae4
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2026-01-09 11:18:25 +0700

    Fix OneSignalSDKWorker.js
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,11 +1,1 @@
-
   <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-  <script>
-    window.OneSignalDeferred = window.OneSignalDeferred || [];
-    OneSignalDeferred.push(async function(OneSignal) {
-      await OneSignal.init({
-        appId: "041f9b26-797f-4248-9a1f-ce2a331c5007",
-      });
-    });
-  </script>
-  
